cd08a698a778beedfb790e6f6482584950f6395e
Allan Barbosa Américo <allanba182@gmail.com>
2019-08-21 09:59:55 -0300

    limpeza de conteudo inutilizado

--- tipo.php
+++ tipo.php
@@ -1,22 +1,3 @@
-<!-- <div class="col-md-9">
-	<div class="container pagina">
-		<div class="row">
-			<div class="col">
-				<h4>Cadastro de Tipo de Equipamento</h4>
-				<hr />
-				<form action="tipo_equipamento.controller.php?acao=inserir" method="post">
-					<div class="form-group">
-						<label>Descrição do Tipo:</label>
-						<input name="tipo" type="text" class="form-control"
-							placeholder="Exemplo: Impressora, Balanças, Relógio de ponto" required>
-					</div>
-					<button class="btn btn-success">Cadastrar</button>
-				</form>
-			</div>
-		</div>
-	</div>
-</div> -->
-
 <?php
 	$acao = recuperar;
 	require './tipo_equipamento.controller.php';
