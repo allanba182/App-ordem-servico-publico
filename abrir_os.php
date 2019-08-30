e86f27581d54ebbc176d2e1afe57916eb47e0d9d
Allan Barbosa Américo <allanba182@gmail.com>
2019-08-30 09:04:05 -0300

    inclucao de script

--- abrir_os.php
+++ abrir_os.php
@@ -13,10 +13,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Abrir O.S. - Ordem de Serviço</title>
 
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/estilo.css">
+    <?php include './includes.php' ?>
 
     <style>
       .card-home 
